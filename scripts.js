bff762d363b9f9ba0b78c71f4076ead49d7187ac
Julian Meltzer <julianpmeltzer@gmail.com>
2018-07-04 19:43:56 -0400

    broke each section in three so that translation doesn't devolve as much

--- scripts.js
+++ scripts.js
@@ -1,8 +1,16 @@
 var passages = [
-    "After slipping off your porch last night, we lost our common language. (It was not saying what we were thinking. It was taking every feeling  and shoving it deep into our pockets like a used tissue.). My hands tucking your hair behind your ears or waving hello became eyes snapped the other way and a text to end it. Just for now you said. Which meant always.",
-    "I came late, fear-shaken and wondering if you were looking out the window down on me, walking under the street-lit nighttime. Wondering if I looked ok but I would check in my phone’s camera on the elevator ride up. You became the only person I had kissed in an elevator, a fact I wouldn’t tell you. I thought it was important. In a couple of weeks I gave up and showed you the picture I accidentally took the first time, where I look so frightened. It made you laugh. If I could make you laugh, I could fall into trusting you.",
-    "Mostly I just wanted to feel special, and sometimes you were good for that (like you were good at making me fear I didn’t look gay enough to be with you; My hair still long, legs still shaved and still wearing skirts). I fell for the stacks of poetry on your desk and your near-condescending wisdom. That was how it was. Hands held when nobody was looking. We liked pretending we were in love which was fun at the time and then hurt like hell for days after. No one tells you how dangerous that is.",
-        "Now I am good at not looking your way when we are in the same room. I admit, I still read your poems (out of vanity, looking for shards of me). Maybe I haven’t learned anything, I’m still using you to make me feel special. You use me too though."
+    "After slipping off your porch last night, we lost our common language.", 
+    "(It was not saying what we were thinking. It was taking every feeling  and shoving it deep into our pockets like a used tissue.).", 
+    "My hands tucking your hair behind your ears or waving hello became eyes snapped the other way and a text to end it. Just for now you said. Which meant always.",
+    "I came late, fear-shaken and wondering if you were looking out the window down on me, walking under the street-lit nighttime. Wondering if I looked ok but I would check in my phone’s camera on the elevator ride up.", 
+    "You became the only person I had kissed in an elevator, a fact I wouldn’t tell you. I thought it was important.", 
+    "In a couple of weeks I gave up and showed you the picture I accidentally took the first time, where I look so frightened. It made you laugh. If I could make you laugh, I could fall into trusting you.",
+    "Mostly I just wanted to feel special, and sometimes you were good for that (like you were good at making me fear I didn’t look gay enough to be with you; My hair still long, legs still shaved and still wearing skirts).", 
+    "I fell for the stacks of poetry on your desk and your near-condescending wisdom. That was how it was. Hands held when nobody was looking.", 
+    "We liked pretending we were in love which was fun at the time and then hurt like hell for days after. No one tells you how dangerous that is.",
+    "Now I am good at not looking your way when we are in the same room.",
+    "I admit, I still read your poems (out of vanity, looking for shards of me).", 
+    "Maybe I haven’t learned anything, I’m still using you to make me feel special. You use me too though."
 ];
 
 function Text (currentText, nextText, ready, queued, count) {
@@ -84,7 +92,7 @@ function update () {
     for (var i = 0; i < sections.length; i++) {//update all sections
         var string = "";
         for (var j = 0; j < sections[i].contains.length; j++){//concatinate texts
-            string += sections[i].contains[j].currentText;
+            string += sections[i].contains[j].currentText + " ";
         }
         sections[i].getElement().innerHTML = string;//replace in docment
     }
@@ -96,24 +104,24 @@ function onLoad () {
     for (var i = 0; i < passages.length; i++){
         texts.push(new Text (passages[i], passages[i], true, false, 2));
     }
-    sections.push (new Section ("text1", [texts[0]]))
-    sections.push (new Section ("text2", [texts[1]]))
-    sections.push (new Section ("text3", [texts[2]]))
-    sections.push (new Section ("text4", [texts[3]]))
+    sections.push (new Section ("text1", [texts[0], texts[1], texts[2]]))
+    sections.push (new Section ("text2", [texts[3], texts[4], texts[5]]))
+    sections.push (new Section ("text3", [texts[6], texts[7], texts[8]]))
+    sections.push (new Section ("text4", [texts[9], texts[10], texts[11]]))
     document.getElementById("translate1").onclick = function () {
-        replaceText([texts [0]], sections[0]);
+        replaceText(sections [0].contains, sections[0]);
         return false;
     };
     document.getElementById("translate2").onclick = function () {
-        replaceText([texts [1]], sections[1]);
+        replaceText(sections [1].contains, sections[1]);
         return false;
     };
     document.getElementById("translate3").onclick = function () {
-        replaceText([texts [2]], sections[2]); 
+        replaceText( sections [2].contains, sections[2]); 
         return false;
     };
     document.getElementById("translate4").onclick = function () {
-        replaceText([texts [3]], sections[3]);
+        replaceText(sections [3].contains, sections[3]);
         return false;
     };
     replaceText (texts, null);
